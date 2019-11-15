f405ddfcc36b3884191aba2238c132574879e3fe
mbosselut <duchmol7@hotmail.com>
2019-11-15 13:42:22 +0100

    Added cors

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 4000;
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
-const Sse = require("json-sse");
+const Sse = require('json-sse');
+const cors = require('cors');
+
+const corsMiddleware = cors();
+
+app.use(corsMiddleware);
 
 app.use(jsonParser);
 
-app.get("/", (req, res, next) => {
-  res.send("hello world");
+app.get('/', (req, res, next) => {
+    res.send('hello world');
 });
 
 const stream = new Sse();
 const messages = [];
 
 app.get('/stream', (req, res, next) => {
-  const string = JSON.stringify(messages);
-  stream.updateInit(string);
-  stream.init(req, res);
-})
-
-app.get("/message", (req, res, next) => {
-  res.send(messages);
+    const string = JSON.stringify(messages);
+    stream.updateInit(string);
+    stream.init(req, res);
 });
 
-app.post("/message", (req, res, next) => {
-  const { message } = req.body;
-  const string = JSON.stringify(message);
-  stream.send(string);
-  messages.push(message);
-  res.send(message);
+app.get('/message', (req, res, next) => {
+    res.send(messages);
+});
+
+app.post('/message', (req, res, next) => {
+    const { message } = req.body;
+    const string = JSON.stringify(message);
+    stream.send(string);
+    messages.push(message);
+    res.send(message);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
