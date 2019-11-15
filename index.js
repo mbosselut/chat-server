ddf8038e6d46876be924fdad74946094531161de
mbosselut <duchmol7@hotmail.com>
2019-11-15 10:26:07 +0100

    Added get and post /message route

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 4000;
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
 
-app.get('/', (req, res, next) => {
-    res.send('hello world')
+app.use(jsonParser);
+
+app.get("/", (req, res, next) => {
+  res.send("hello world");
+});
+
+const messages = [];
+
+app.get('/message', (req, res, next) => {
+  res.send(messages);
 })
+
+app.post("/message", (req, res, next) => {
+  const { message } = req.body;
+  messages.push(message);
+  res.send(message);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
