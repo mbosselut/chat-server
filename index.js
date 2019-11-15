0e9b22117a49aff8c63ec798f3069bb6613de733
mbosselut <duchmol7@hotmail.com>
2019-11-15 15:13:00 +0100

    Added different streams

--- index.js
+++ index.js
@@ -13,28 +13,59 @@ app.use(corsMiddleware);
 app.use(jsonParser);
 
 app.get('/', (req, res, next) => {
-    res.send('hello world');
+	res.send('hello world');
 });
 
 const stream = new Sse();
-const messages = [];
+// DB would be used instead of 'rooms' or 'messages'
+const messages = {};
 
 app.get('/stream', (req, res, next) => {
-    const string = JSON.stringify(messages);
-    stream.updateInit(string);
-    stream.init(req, res);
+	const rooms = Object.keys(messages);
+	const string = JSON.stringify(rooms);
+	stream.updateInit(string);
+	stream.init(req, res);
+});
+
+app.get('/rooms/:roomName', (req, res, next) => {
+	const { roomName } = request.params;
+	const stream = streams[roomName];
+	// const messages = {fun : ['hi', 'hi there'], room2: ['coucou']}
+	const data = messages[roomName];
+	// data === ['hi', 'hi there']
+	const string = JSON.stringify(data);
+	stream.updateInit(string);
+	stream.init(req, res);
+});
+
+function send(data) {
+	const string = JSON.stringify(data);
+	stream.send(string);
+}
+
+app.post('/room', (req, res, next) => {
+	const { name } = req.body;
+	send(name);
+	rooms.push(name);
+	messages[name] = [];
+	streams[name] = new Sse();
+	res.send(name);
 });
 
 app.get('/message', (req, res, next) => {
-    res.send(messages);
+	res.send(messages);
 });
 
-app.post('/message', (req, res, next) => {
-    const { message } = req.body;
-    const string = JSON.stringify(message);
-    stream.send(string);
-    messages.push(message);
-    res.send(message);
+app.post('/message/:roomName', (req, res, next) => {
+	const { message } = req.body;
+	const { roomName } = request.params;
+	const room = messages[roomName];
+	room.push(message);
+	const stream = streams[roomName];
+	const string = JSON.stringify(message);
+	stream.send(string);
+	// messages.push(message);
+	res.send(message);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
