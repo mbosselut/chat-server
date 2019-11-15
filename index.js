b5d4da067e268b1166fecae2c7edf3bb1839e63b
mbosselut <duchmol7@hotmail.com>
2019-11-15 10:38:30 +0100

    Setting up stream

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 4000;
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
+const Sse = require("json-sse");
 
 app.use(jsonParser);
 
@@ -10,11 +11,18 @@ app.get("/", (req, res, next) => {
   res.send("hello world");
 });
 
+const stream = new Sse();
 const messages = [];
 
-app.get('/message', (req, res, next) => {
-  res.send(messages);
+app.get('/stream', (req, res, next) => {
+  const string = JSON.stringify(messages);
+  stream.updateInit(string);
+  stream.init(req, res);
 })
+
+app.get("/message", (req, res, next) => {
+  res.send(messages);
+});
 
 app.post("/message", (req, res, next) => {
   const { message } = req.body;
